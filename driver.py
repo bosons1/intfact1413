34eb19e7c925d1098983f71bc485de58a85174bb
Subhendra Basu <subhendra.one@gmail.com>
2025-02-24 12:31:54 +0000

    code

--- driver.py
+++ driver.py
@@ -1,16 +1,29 @@
 #!/usr/bin/python3
 
 import sys
+import subprocess
 
 if __name__ == "__main__":
     num = str(sys.argv[1])
     print(num)
     zero_index = 1
     while True:
-        # call intfact1 with num and zero index
-        # call intfact2 with num and zero index
-        # call intfacta with num and zero index
-        # call intfactaa with num and zero index
+        process = subprocess.Popen(["./intfact1", num, str(zero_index)], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        stdout, stderr = process.communicate()
+        print(stdout.decode())
+        print(stderr.decode())
+        process = subprocess.Popen(["./intfact2", num, str(zero_index)], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        stdout, stderr = process.communicate()
+        print(stdout.decode())
+        print(stderr.decode())
+        process = subprocess.Popen(["./intfacta", num, str(zero_index)], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        stdout, stderr = process.communicate()
+        print(stdout.decode())
+        print(stderr.decode())
+        process = subprocess.Popen(["./intfactaa", num, str(zero_index)], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        stdout, stderr = process.communicate()
+        print(stdout.decode())
+        print(stderr.decode())
         # find symmetry and anti symmetry
         #if symmetry found decode using Riemann zetazero
         zero_index = zero_index + 1
