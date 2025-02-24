3c71196d0a877e86570608040ce176f3344cf063
Subhendra Basu <subhendra.one@gmail.com>
2025-02-24 12:26:05 +0000

    code

---
+++ driver.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+import sys
+
+if __name__ == "__main__":
+    num = str(sys.argv[1])
+    print(num)
+    zero_index = 1
+    while True:
+        # call intfact1 with num and zero index
+        # call intfact2 with num and zero index
+        # call intfacta with num and zero index
+        # call intfactaa with num and zero index
+        # find symmetry and anti symmetry
+        #if symmetry found decode using Riemann zetazero
+        zero_index = zero_index + 1
+        input("Enter")
