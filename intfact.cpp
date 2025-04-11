73b2fc25efb69fe5ac67eb449a1daae268144574
Subhendra Basu <subhendra.one@gmail.com>
2025-04-11 09:39:04 +0000

    code

--- intfact.cpp
+++ intfact.cpp
@@ -12,6 +12,7 @@
 #include <flint/fmpz.h>
 #include <gmp.h>
 #include "zeros.hpp"
+#include "primes.hpp"
 #include <bits/stdc++.h>
 #define PREC 16384
 #define TOLERANCE 10
@@ -50,14 +51,49 @@ char* get_zero(int zero_index, int prec=PREC) {
 	return zero;
 }
 
-bool isZero(int x) {
+bool isZero(int x, int& d) {
 	std::vector<int>::iterator it = std::find(zeros.begin(), zeros.end(), x);
 	if (it == zeros.end()) {
 		return false;
 	} else {
+		d = std::distance(zeros.begin(), it);
 		return true;
 	}
 	return false;
+}
+
+bool isPrime(int x, int& d) {
+	std::vector<int>::iterator it = std::find(primes.begin(), primes.end(), x);
+	if (it == primes.end()) {
+		return false;
+	} else {
+		d = std::distance(primes.begin(), it);
+		return true;
+	}
+	return false;
+}
+
+void* characterize(string num, int divisor, std::string& ss) {
+   int sum1 = 0;
+   long int c = 0;
+   long int l = num.length();
+   int count = 0;
+   while (1) {
+	   char nn = num[c % l];
+	   int nk = nn - '0';
+	   sum1 += nk;
+	   ++c;
+	   if (sum1 % divisor == 0) {
+		   count = sum1 / divisor;
+		   ss += boost::lexical_cast<std::string>(count);
+		   sum1 = 0;
+                   if (c % l == 0) {
+			   break;
+		   }
+	   }
+   }
+   std::reverse(ss.begin(), ss.end());
+   return 0;
 }
 
 int main(int argc, char* argv[]) {
@@ -69,10 +105,14 @@ int main(int argc, char* argv[]) {
 	std::string c29 = "";
 	characterize(num, 17, c17);
 	characterize(num, 29, c29);
+	cout << c17 << endl;
+	cout << c29 << endl;
 	int d1 = 0;
 	bool bIsZero1 = isZero(atoi((char*)c17.c_str()), d1);
+	cout << "d1 " << d1 << endl;
 	int d2 = 0;
 	bool bIsZero2 = isZero(atoi((char*)c29.c_str()), d2);
+	cout << "d1 " << d1 << endl;
 	int zero_index = d1;
 	char* zero = get_zero(zero_index);
 	FILE* pi = fopen64("./pi.txt","r");
@@ -83,21 +123,21 @@ int main(int argc, char* argv[]) {
 	unsigned long long int c = 0;
 	while (1) {
 		char pp = 0, ee = 0;
-		fscanf(pp, "%c", &pp);
-		fscanf(ee, "%c", &ee);
+		fscanf(pi, "%c", &pp);
+		fscanf(e, "%c", &ee);
 		char zz = zero[zero_pos++];
 		char test[4];
 		test[0] = pp;
-		test[1] = nn;
+		test[1] = zz;
 		test[2] = ee;
 		test[3] = '\0';
 		int tk = atoi(test);
 		int d =0;
-		bool bIsPrime1 = isPrime(tk, &d);
+		bool bIsPrime1 = isPrime(tk, d);
 		test[0] = ee;
 		test[2] = pp;
 		tk = atoi(test);
-		bool bIsPrime2 = isPrime(tk, &d);
+		bool bIsPrime2 = isPrime(tk, d);
 		++c;
 		if (bIsPrime1 && bIsPrime2) {
 			printf("zz %c\n", zz);
