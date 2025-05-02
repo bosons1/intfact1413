7c47d9ee83f95f36affa9337c5fa29bce40ffa46
Subhendra Basu <subhendra.one@gmail.com>
2025-05-02 05:44:58 +0000

    code

--- intfact.cpp
+++ intfact.cpp
@@ -40,11 +40,12 @@ int main(int argc, char* argv[]) {
 	mpz_set_str(nz, num, 10);
 	unsigned long long int c = 0, count = 0;
 	std::string champernowne = "0";
+	std::string factor = "";
 	while (1) {
 		char* ns = strdup(mpz_get_str(0, 10, nz));
 		long int l = strlen(ns);
-		fseek(pi, l, SEEK_SET);
-		fseek(e, l, SEEK_SET);
+		fseek(pi, l, SEEK_CUR);
+		fseek(e, l, SEEK_CUR);
 		char pp = 0, ee = 0;
 		fscanf(pi, "%c", &pp);
 		fscanf(e, "%c", &ee);
@@ -61,13 +62,18 @@ int main(int argc, char* argv[]) {
 			while (pos > champernowne.length()) {
 			    champernowne += boost::lexical_cast<std::string>(++c);
 			}
-		        if (pos == champernowne.length()) {
+			long int ll = champernowne.length();
+		        if (pos == ll) {
+			    char last = champernowne[ll - 1];
+			    factor += boost::lexical_cast<std::string>(last);
 			    ++count;
 			    if (count == l) break;
 			    bool bIsPrime_c = isPrime(c);
 			    if (bIsPrime_c) {
-			        fseek(pi, OFFSET, SEEK_SET);
-			        fseek(e, OFFSET, SEEK_SET);
+				    fseek(pi, OFFSET, SEEK_SET);
+				    fseek(e, OFFSET, SEEK_SET);
+				    mpz_add_ui(nz, nz, 1);
+				    continue;
 			    }
 			} 	
 		}
@@ -77,6 +83,7 @@ int main(int argc, char* argv[]) {
 	fclose(pi);
 	fclose(e);
 	gettimeofday(&end, NULL);
+	cout << factor << endl;
 	double time_taken = (end.tv_sec-start.tv_sec) + (end.tv_usec-start.tv_usec) / 1e6;
 	printf("Total time taken is %f seconds\n", time_taken);
 }
